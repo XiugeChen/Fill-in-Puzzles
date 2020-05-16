c92482c2c80e17aa6e381073d6300cf9393315aa
Xiuge Chen <xiugec@student.unimelb.edu.au>
2020-05-16 11:05:11 +1000

    add functions to support sorting words by the number of words that has the same length

--- src/proj2.pl
+++ src/proj2.pl
@@ -7,6 +7,39 @@
 
 puzzle_solution(Puzzle, WordList).
 
+% 
+sort_by_len_occur(Lst, SortGpLst) :-
+    sort_by_len(Lst, SortLst),
+    group_pairs_by_key(SortLst, GpLst),
+    remove_key(GpLst, GpLst1), 
+    sort_by_len(GpLst1, SortGpLst0),
+    remove_key(SortGpLst0, SortGpLst1),
+    down_dim(SortGpLst1, SortGpLst).
+
+% 
+sort_by_len(Lst, SortLst) :-
+    gen_len_key(Lst, LstLen),
+    keysort(LstLen, SortLst).
+
+%
+gen_len_key([], []).
+gen_len_key([X|Xs], [Y|Ys]) :-
+    length(X, Len),
+    Y = Len-X,
+    gen_len_key(Xs, Ys).
+
+% 
+remove_key([], []).
+remove_key([_-Y|Xs], [Y|Ys]) :-
+    remove_key(Xs, Ys).
+
+%
+down_dim([], []).
+down_dim([[]|Xss], Ys) :-
+    down_dim(Xss, Ys).
+down_dim([[X|Xs]|Xss], [X|Ys]) :-
+    down_dim([Xs|Xss], Ys).
+
 %
 insert_word(Matrix, Word) :-
     insert_to_matrix(Matrix, Word) ;
