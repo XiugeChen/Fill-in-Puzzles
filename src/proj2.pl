b7a49c9ec6ca83353f3fcef2c0a72c9e7f089d88
Xiuge Chen <xiugec@student.unimelb.edu.au>
2020-05-16 17:27:51 +1000

    solve test 2, 3, 4

--- src/proj2.pl
+++ src/proj2.pl
@@ -5,7 +5,9 @@
 % ensure the program load the correct library for matrix transpose
 :- ensure_loaded(library(clpfd)).
 
-puzzle_solution(Puzzle, WordList).
+puzzle_solution(Puzzle, WordList) :-
+    sort_by_len_occur(WordList, WordList1),
+    insert(Puzzle, WordList1).
 
 % 
 sort_by_len_occur(Lst, SortGpLst) :-
@@ -40,6 +42,12 @@ down_dim([[]|Xss], Ys) :-
 down_dim([[X|Xs]|Xss], [X|Ys]) :-
     down_dim([Xs|Xss], Ys).
 
+% 
+insert(_, []).
+insert(Matrix, [Word|Words]) :-
+    insert_word(Matrix, Word),
+    insert(Matrix, Words).
+
 %
 insert_word(Matrix, Word) :-
     insert_to_matrix(Matrix, Word) ;
@@ -54,14 +62,20 @@ insert_to_matrix([Row|Matrix], Word) :-
     insert_to_matrix(Matrix, Word).
 
 % 
-insert_to_row([], []).
-insert_to_row([R|Rs], []) :-
-    ( R == #
-    -> insert_to_row(Rs, [])
-    ).
-insert_to_row([R|Rs], [W|Ws]) :-
-    (   R == #
-    ->  insert_to_row(Rs, [W|Ws])
-    ;   R = W
-    ->  insert_to_row(Rs, Ws)
-    ).
+insert_to_row(Row, Word) :-
+        Word = Row
+    ;
+    (   append(Word, [X|_], Row), 
+        nonvar(X) )
+    ;
+    (   append(Lst, Word, Row),
+        append(_, [X], Lst),
+        nonvar(X) )
+    ;
+    (   append(Front, [X|_], Row),
+        nonvar(X),
+        append(Lst, Word, Front),
+        append(_, [Y], Lst),
+        nonvar(Y)
+    )
+    .
