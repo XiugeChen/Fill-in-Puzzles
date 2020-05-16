cf8f3fafd47e70d2617d9df0a0388180a9408ba8
Xiuge Chen <xiugec@student.unimelb.edu.au>
2020-05-16 10:23:45 +1000

    add matrix insertion predicate

--- src/proj2.pl
+++ src/proj2.pl
@@ -1,3 +1,34 @@
 % Author:         Xiuge Chen <xiugec@student.unimelb.edu.au>
 % Created on: 2020.05.15
 % Last Modified on: 2020.05.15
+
+% ensure the program load the correct library for matrix transpose
+:- ensure_loaded(library(clpfd)).
+
+puzzle_solution(Puzzle, WordList).
+
+%
+insert_word(Matrix, Word) :-
+    insert_to_matrix(Matrix, Word) ;
+    (   transpose(Matrix, MatrixT),
+        insert_to_matrix(MatrixT, Word),
+        transpose(MatrixT, Matrix)
+    ).
+
+%
+insert_to_matrix([Row|Matrix], Word) :-
+    insert_to_row(Row, Word) ;
+    insert_to_matrix(Matrix, Word).
+
+% 
+insert_to_row([], []).
+insert_to_row([R|Rs], []) :-
+    ( R == #
+    -> insert_to_row(Rs, [])
+    ).
+insert_to_row([R|Rs], [W|Ws]) :-
+    (   R == #
+    ->  insert_to_row(Rs, [W|Ws])
+    ;   R = W
+    ->  insert_to_row(Rs, Ws)
+    ).
